538403f3622a75e10347fb91b94d28279da45b65
Mikael Finstad <finstaden@gmail.com>
2023-07-14 12:39:11 +0200

    remove dependency on config

--- index.js
+++ index.js
@@ -1,16 +1,11 @@
 'use strict';
 
-const config = require('eslint-config-transloadit');
-
 module.exports = {
 	meta: {
 		name: 'eslint-plugin-transloadit',
 		// x-release-please-start-version
 		version: '0.1.2',
 		// x-release-please-end
-	},
-	configs: {
-		config,
 	},
 	rules: {
 		'no-useless-iife': require('./rules/no-useless-iife'),
